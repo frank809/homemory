893fffc0a42201dc01148af295d535320a8f2d5c
Liu Frank <frank.2.liu@nokia.com>
2017-07-21 11:12:01 +0800

    add db method

--- dbmanager/dbmanager.py
+++ dbmanager/dbmanager.py
@@ -2,42 +2,54 @@
 # -*- coding: utf-8 -*-
 
 import sqlite3
+import os.path
 
 
 class dbmanager(object):
 
     def __init__(self, db="./init.db"):
-        '''
+        """
         SELECT count(*) FROM sqlite_master WHERE type='table' AND name='要查询的表名';
         :param db:
-        '''
-        self.conn = sqlite3.connect(db)
-        self.initphototable()
-        self.initeventable()
+        """
+        if os.path.exists(db):
+            self.conn = sqlite3.connect(db)
+            self.dbcursor = self.conn.cursor()
+            self.initphototable(True)
+        else:
+            self.conn = sqlite3.connect(db)
+            self.dbcursor = self.conn.cursor()
+            self.initphototable(False)  #need create tables
 
-    def initphototable(self):
-        '''
+
+
+    def initphototable(self,isdbexist):
+        """
         CREATE TABLE photos (id string , rawfilepath string, glancefilepath string, createtime string,
          gps string, eventid string, persons string, PRIMARY KEY('id'))
         :return:
-        '''
-        pass
+        """
+        if isdbexist:  #if db exist then check table and try one select.
+            pass  #todo: need verify something when db exist.
+        else:
+            self.dbcursor.execute("CREATE TABLE photos (id string , rawfilepath string, glancefilepath string, "
+                                  "createtime string,gps string, eventid string, persons string, PRIMARY KEY('id'))")
 
     def initeventable(self):
         pass
 
-    def get_photoinfo(self, photoid="all"):
-        '''
+    def get_photoinfo(photoid="all"):
+        """
         method used for getting photo infomation. all informations include (rawfilepath, glancefilepath, createtime,
         gps, eventid, persons) would be return. param photoid is all by default. means it will return all stored file.
-        TODO: think about if too many photos and get in separate pages?
         :param photoid:all=get all photos informatin. specific id means get one.
         :return:
-        '''
+        """
+        #todo: think about if too many photos and get in separate pages?
         pass
 
     def put_photoinfo(self, photoid, rawfilepath, glancefilepath, createtime="", gpsinfo="", eventid="", persons=""):
-        '''
+        """
         :param photoid:
         :param rawfilepath:
         :param glancefilepath:
@@ -46,12 +58,14 @@ class dbmanager(object):
         :param eventid:
         :param persons:
         :return:
-        '''
-        pass
+        """
+        sqlstring="insert into photos values ('%s','%s','%s','%s','%s','%s','%s')"%(photoid,rawfilepath,glancefilepath,createtime,gpsinfo,eventid,persons)
+        self.dbcursor.execute(sqlstring)
+        self.conn.commit()
 
     def update_photoinfo(self, photoid, rawfilepath="", glancefilepath="", createtime="", gpsinfo="", eventid="", persons=""):
-        '''
-
+        """
+        update photos set gpsinfo = "aaa" where photoid ="ffff";
         :param photoid:
         :param rawfilepath:
         :param glancefilepath:
@@ -60,10 +74,12 @@ class dbmanager(object):
         :param eventid:
         :param persons:
         :return:
-        '''
+        """
+        #todo: update some of value should be supported.
         pass
 
     def __del__(self):
+        self.conn.close()
         pass
 
 
---
+++ filemanager/photo.py
@@ -0,0 +1,9 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+import PIL
+
+class jpgphoto (object):
+
+    def __init__(self):
+        pass
