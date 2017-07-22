f5cef72c2a5e9a5aeea2aaa804adbad62d8d6ed3
Liu Frank <frank.2.liu@nokia.com>
2017-07-22 21:36:09 +0800

    add test and modify files and photo

--- filemanager/files.py
+++ filemanager/files.py
@@ -1,2 +1,38 @@
 #! /usr/bin/python
 # -*- coding: utf-8 -*-
+
+from PIL import Image
+
+class filemanager(object):
+
+    def __init__(self):
+        pass
+
+    def __del__(self):
+        pass
+
+    def tidy(self,inputfolder):
+        """
+        for item in inputfolder:
+            if folder then recurrence.
+            if jpeg files then dill with get_exifinfo() and then save info to db. id should be SHA256 of photo files.
+                moveRAWfile()
+                createThumbfile()
+        :param inputfolder:
+        :return:
+        """
+        pass
+
+    def get_info(self,file):
+        """
+
+        :param file:
+        :return:
+        """
+        shavalue=""
+        createtime=""
+        gps=""
+        img = Image.open(file)
+
+
+        return (shavalue, createtime, gps)
--- filemanager/photo.py
+++ filemanager/photo.py
@@ -1,9 +1,25 @@
 #! /usr/bin/python
 # -*- coding: utf-8 -*-
 
-import PIL
+from PIL import Image
 
-class jpgphoto (object):
+class photo (object):
 
     def __init__(self):
+        pass
+
+    def getfromjpeg(self,file):
+        """
+        获取jpeg文件的exif信息。只需要返回sha256值，创建时间，GPS信息构成的字典
+        :param file:
+        :return:
+        """
+        pass
+
+    def createthumb(self, file, outputfolder):
+        """
+        创建文件对应的thumb文件。并存放都某个特定目录
+        :param file:
+        :return:
+        """
         pass
